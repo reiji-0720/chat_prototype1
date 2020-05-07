02a50da4e5638cb60550cee7dec088fcd34422b7
REiJi <shino327gh@gmail.com>
2020-05-07 11:41:32 +0900

    firestoreで読み書きできる処理を追加した。

--- add_chatRoom/firebase.js
+++ add_chatRoom/firebase.js
@@ -1,9 +1,7 @@
-const firebase = require("firebase");
-// Required for side-effects
-require("firebase/firestore");
+
 
 // Initialize Cloud Firestore through Firebase
-firebase.initializeApp({
+var config={
     apiKey: "AIzaSyAlaRSn1Q0XjUaxXaxYJV3aOp_5OrpaGE4",
     authDomain: "addcategory.firebaseapp.com",
     databaseURL: "https://addcategory.firebaseio.com",
@@ -11,19 +9,8 @@ firebase.initializeApp({
     storageBucket: "addcategory.appspot.com",
     messagingSenderId: "534782526050",
     appId: "1:534782526050:web:6ae11f42235a13cf44c73b"
-  });
+  };
   
-  var db = firebase.firestore();
+  firebase.initializeApp(config);
 
 
-  db.collection("users").add({
-    first: "Ada",
-    last: "Lovelace",
-    born: 1815
-})
-.then(function(docRef) {
-    console.log("Document written with ID: ", docRef.id);
-})
-.catch(function(error) {
-    console.error("Error adding document: ", error);
-});
