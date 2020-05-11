5ca7f00c4937a47f1603fc4c1b0a033114bace33
REiJi <[shino327gh@gmail.com]>
2020-05-12 01:16:48 +0900

    新規ルーム作成用のform作成

---
+++ add_room/main.js
@@ -0,0 +1,16 @@
+
+
+// Initialize Cloud Firestore through Firebase
+var config={
+    apiKey: "AIzaSyAlaRSn1Q0XjUaxXaxYJV3aOp_5OrpaGE4",
+    authDomain: "addcategory.firebaseapp.com",
+    databaseURL: "https://addcategory.firebaseio.com",
+    projectId: "addcategory",
+    storageBucket: "addcategory.appspot.com",
+    messagingSenderId: "534782526050",
+    appId: "1:534782526050:web:6ae11f42235a13cf44c73b"
+  };
+  
+  firebase.initializeApp(config);
+
+
